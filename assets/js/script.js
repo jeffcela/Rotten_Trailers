15cbc61a7ab61a369fa70f6d23432224e2bc2e77
Hungry Man <katsu.matsushita@gmail.com>
2021-07-31 10:24:05 -0400

    Added the IMDB-Alternative API keys to the js file. Documentation can be found at: https://rapidapi.com/rapidapi/api/movie-database-imdb-alternative/

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+fetch("https://movie-database-imdb-alternative.p.rapidapi.com/?i=tt4154796&r=json", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "e3ff15896amsh9db6024de9b3d2ap1a6df9jsn7a0527f74de8",
+		"x-rapidapi-host": "movie-database-imdb-alternative.p.rapidapi.com"
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
+});
