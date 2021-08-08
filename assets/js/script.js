0dd6a1d046ca006b62989efdc3cede710dd2b7d3
Hannahdlewis <hannahdlewis07@gmail.com>
2021-08-08 19:54:46 -0400

    removed options from code not being used in our code

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,5 +2,5 @@
 
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, options);
+    var instances = M.Sidenav.init(elems);
   });
