216322d2e17628ce94e45ae2f00d8a6c3e80e577
Galatia <ptrivedius@aol.com>
2021-08-09 14:01:24 -0400

    to push code with trailer playing

--- assets/js/video.js
+++ assets/js/video.js
@@ -1,29 +1,30 @@
-
 var apiKey = 'AIzaSyC46Ga2DC556_V4JYsLVaCkqzQz0D_Roaw';
-var url = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&type=video&q=dune&videotype=movie&key=${apiKey}`;
+//var url = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&type=video&q=dune&videotype=movie&key=${apiKey}`;
+var fandangoChannel = "UCi8e0iOVk1fEOogdfu4YgfA";
 
-
-
-// GET https://www.googleapis.com/youtube/v3/search
-
+// Loads code for iPlayer API
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 // Get the modal
 var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("btn-search");
-
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
-function showTrailer() {
-  modal.style.display = "block";
-
-  // replace placeholder video with trailer
-}
-
-function callYouTube () {
+function callYouTube (trailerBtn) {
+  var urlEl1 = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCi8e0iOVk1fEOogdfu4YgfA&maxResults=10&q=`;
+  var urlElQuery = trailerBtn.dataset.movie; // this actually grabs the name of the movie from the button
+  //var urlElQuery = "Dune"; //this is a variable, it has to get set when the callYouTube function gets called
+  var urlEl2 = `&key=` + apiKey;
+  var url = urlEl1 + urlElQuery + urlEl2;
+  
+  // this is being left here for future debugging purposes
+  //console.log("from inside callYouTube: " + url);
+  //console.log(trailerBtn.dataset.movie);
+  
   fetch(url)
   .then(response => {
       //console.log(response);
@@ -33,31 +34,23 @@ function callYouTube () {
   .then(data => {
       // returns the search results object
       console.log(data);
-      console.log(data.items[0].snippet.channelId); 
-      var videoIdentifier = data.items[0].videoId;
-      // document.getElementById("testing").setAttribute("")
+      //console.log(data.items[0].snippet.channelId); 
+      var videoIdentifier;
+      if (data.items[0].snippet.channelId == fandangoChannel){
+        videoIdentifier = data.items[0].id.videoId;
+      } else {
+        alert("No trailer found!");
+      };
+      //console.log(videoIdentifier);
+      modal.style.display = "block";
+      player.loadVideoById(videoIdentifier);
   })
 }
 
-/* async function getTrailer() {
-  if (data.items[0].snippet.channelId === UCi8e0iOVk1fEOogdfu4YgfA) {
-
-  }
-  if (data.items[0].snippet.channelTitle = )
-} */
-
-
-
-
-// Loads code for iPlayer API
-var tag = document.createElement('script');
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
 // Creates iFrame and YouTube player
 var player;
-function onYouTubeIframeAPIReady(event) {
+function onYouTubeIframeAPIReady() {
+  console.log("API ready, preparing iFrame");
   player = new YT.Player('player', {
     height: '390',
     width: '640',
@@ -66,23 +59,18 @@ function onYouTubeIframeAPIReady(event) {
       'playsinline': 1
     },
     events: {
-      'onReady': onPlayerReady(event),
-      'onStateChange': onPlayerStateChange
+      'onReady': onPlayerReady,
+      //'onStateChange': onPlayerStateChange
     }
   });
+  console.log(player);
 }
 
 // API calls function when player ready
 function onPlayerReady(event) {
-  event.target.playVideo(event);
+  //event.target.playVideo();
+  console.log("onReady event has fired");
 }
-
-
-
-function constructVideoUrl(videoIdentifier) {
-  var videoUrl; // needs to be `https://www.youtube.com/embed/${videoIdentifier}`;
-}
-
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
@@ -95,4 +83,5 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
 
